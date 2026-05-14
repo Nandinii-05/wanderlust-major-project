d48129193eeec770f504be9895840bbf50e99851
Nandinii-05 <singhkhushi262605@gmail.com>
2026-05-14 19:18:19 +0530

    DB name fixed

--- recommendation/db.py
+++ recommendation/db.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 client = MongoClient(os.getenv("MONGO_URI"))
-db = client.get_default_database()
+db = client["test"]
 
 listings_col = db["listings"]        # mongoose lowercases + pluralizes → "listings"
 interactions_col = db["interactions"]
